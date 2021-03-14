c5bc34a83a079bafb9d1843f96ffccc3f12282ff
sarmad Al-Bidhawi <sarmed.91@hotmail.com>
2021-03-14 14:39:42 +0100

    user model

---
+++ models/user.js
@@ -0,0 +1,23 @@
+const mongoose = requrie('mongoose')
+const UserSchema = new mongoose.Schema({
+    name : {
+        type: String,
+        require: true
+    },
+    email :{
+        type  : String,
+        required : true
+    } ,
+    password :{
+        type  : String,
+        required : true
+    } ,
+    date :{
+        type : Date,
+        default : Date.now
+    }
+})
+
+const User = mongoose.model('User, UserSchema')
+
+module.exports = User;
