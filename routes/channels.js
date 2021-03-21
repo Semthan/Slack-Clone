0473d22421132116507493c61b5381a1febe2ab6
sarmad Al-Bidhawi <sarmed.91@hotmail.com>
2021-03-22 00:47:44 +0100

    moved ch crud from index to channels

--- routes/channels.js
+++ routes/channels.js
@@ -6,6 +6,31 @@ router.use(express.urlencoded({ extended: true }));
 
 const Channel = require('../models/channel');
 
+router.get('/createChannel', (req,res)=>{
+  res.render('createChannel');
+})
+
+router.post('/createChannel', (req,res) => {
+  const channel = new Channel({
+      name: req.body.name,
+      description: req.body.description || ''
+  })
+
+  channel.save((error) => {
+      if (error) return console.error(error)
+      console.log('Channel created.');
+      res.redirect('/dashboard');
+  })
+})
+
+router.get('/delete/:id', (req,res) => {
+  Channel.deleteOne({_id: req.params.id}, (err,data) => {
+      if (err) return console.error(err);
+      res.redirect('/dashboard');
+  })
+})
+
+
 router.get('/:id', (req,res) => {
   Channel.findById(req.params.id, (error,data) => {
     if (error) return console.error(error);
--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const router  = express.Router();
-const {ensureAuthenticated} = require("../config/auth")
+const {ensureAuthenticated} = require("../config/auth");
+const Channel = require('../models/channel');
+
+router.use(express.urlencoded({ extended: true }));
 
 //login page
 router.get('/', (req,res)=>{
-    res.render('welcome');
+        res.render('welcome');
 })
 
 //register page
@@ -12,13 +15,18 @@ router.get('/register', (req,res)=>{
     res.render('register');
 })
 
+
 //dashboard
 router.get('/dashboard',ensureAuthenticated,(req,res)=> {
-    res.render('dashboard', { user: req.user})
+    Channel.find((error,data) => {
+        if (error) return console.error(error)
+        res.render('dashboard', { user: req.user, channels: data})
+    })
+
 })
 
-router.get('/chat', ensureAuthenticated,(req,res)=>{
+/* router.get('/chat', ensureAuthenticated,(req,res)=>{
     res.render('chat', { user: req.user})
-})
+}) */
 
 module.exports = router; 
